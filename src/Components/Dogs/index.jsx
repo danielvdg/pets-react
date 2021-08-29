6f1b23f72cdc8b51af185e810d629cf508d91083
danielvdg <daniel.vdg@hotmail.com>
2021-08-29 17:11:38 -0300

    implementando tabela de dogs

--- src/Components/Dogs/index.jsx
+++ src/Components/Dogs/index.jsx
@@ -1,30 +1,83 @@
-import React,{useEffect, useState} from 'react'
-import * as S from './style'
+import React, { useEffect, useState } from "react";
+import * as S from "./style";
 const Dogs = () => {
+  const [dadosApi, SetDadosApi] = useState([]);
+  const [apiChegou, setApiChegou] = useState(false);
+  const [modalAberto, setModalAberto] = useState(false);
+  const [dogDaVez, setDogDaVez] = useState({})
+  const [dogEhEdicao, setDogEhEdicao] = useState(false)
 
-    const [dadosApi,SetDadosApi] = useState([])
-    const [apiChegou,setApiChegou] = useState(false)
+  useEffect(() => {
+    fetch("https://salty-harbor-83280.herokuapp.com/pets/dogs")
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        SetDadosApi(dados.results);
+        setApiChegou(true);
+      });
 
-    useEffect(()=>{
-        fetch('https://salty-harbor-83280.herokuapp.com/pets/dogs')
-        .then((resposta)=> resposta.json())
-        .then((dados)=> {SetDadosApi(dados.results)
-        setApiChegou(true)}
-        )
-        
-        console.log(dadosApi);
-    },[])
-    
     console.log(dadosApi);
-    return (
-        <S.Container>
-            {!!dadosApi && apiChegou ? (
-                dadosApi.map((item)=>{
-                    console.log(item.nome);
-                })
-            ) : (<p>Carregando</p>)}
-        </S.Container>
-    )
-}
+  }, []);
+
+  console.log(dadosApi);
+  return (
+    <S.Container>
+      <S.Table>
+        <thead>
+          <tr>
+            <th>nome</th>
+            <th>raça</th>
+            <th>idade</th>
+            <th>gênero</th>
+          </tr>
+        </thead>
+        <tbody>
+          {!!dadosApi ? (
+            dadosApi.map((dog) => {
+              return (
+                <tr onClick={() => {
+                    setModalAberto(true)
+                    setDogDaVez(dog)
+                    setDogEhEdicao(true)
+                }} key={dog.id}>
+                  <td>{dog.nome}</td>
+                  <td>{dog.raca}</td>
+                  <td>{dog.idade > 0 ? dog.idade : "Recem nascido"}</td>
+                  <td>{dog.genero === "male" ? "Macho " : "Femea"}</td>
+                </tr>
+              );
+            })
+          ) : (
+            <p>carregando</p>
+          )}
+        </tbody>
+      </S.Table>
+      {modalAberto && (
+        <S.Modal>
+          <S.ModalContent>
+            <button onClick={() => setModalAberto(false)}>x</button>
+            <button onClick={()=> setDogEhEdicao()}>Editar</button>
+            <input type='text' value={dogDaVez.nome} disabled={true} onChange={(e)=>{
+                const value = e.target.value
+                setDogDaVez({nome:value,...dogDaVez})
+            }}/>
+            <input type='text' value={dogDaVez.idade} disabled={true} onChange={(e)=>{
+                const value = e.target.value
+                setDogDaVez({idade:value,...dogDaVez})
+            }} />
+            <input type='text' value={dogDaVez.genero} disabled={true} onChange={(e)=>{
+                const value = e.target.value
+                setDogDaVez({genero:value,...dogDaVez})
+            }}/>
+            <input type='text' value={dogDaVez.raca} disabled={true} onChange={(e)=>{
+                const value = e.target.value
+                setDogDaVez({raca:value,...dogDaVez})
+            }}/>
+            <button onClick={()=> console.log(dogDaVez)}>Salvar </button>
+          </S.ModalContent>
+        </S.Modal>
+      )}
+    </S.Container>
+  );
+};
 
 export default Dogs;
--- src/Components/Dogs/style.js
+++ src/Components/Dogs/style.js
@@ -1,7 +1,63 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-width: 100%;
-height: 100%;
+    width: 100%;
+    height:calc(100vh - 100px);
+    padding: 20px;
+    overflow-y: scroll;
 
 ` 
+
+export const Table = styled.table`
+    width: 95%;
+    height: 85%;
+    border-collapse: collapse;
+    th,td{
+        text-align: center;
+        line-height: 50px;
+        border: 1px solid;
+    }
+
+`
+export const Modal = styled.div`
+    width: 100vw;
+    height: 100vh;
+    
+    display:block;
+    position: absolute;
+    top: 0;
+    background-color: rgba(0,0,0,0.03);
+    z-index: 2;
+    
+    
+
+
+ 
+
+`
+export const ModalContent = styled.div`
+    width: 50%;
+    height: 80%;
+    margin:  30px auto;
+    background-color: white;
+   >img{
+        width: 50px;
+        height: 60px;
+   }
+   >input{
+       width: 40%;
+       height: 30px;
+       padding: 2px;
+       border:  1px solid;
+
+   }
+
+   >button{
+       border: 1px solid;
+       width: 50px;
+       height: 30px;
+
+   }
+`
+
+
